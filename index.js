bb76888c6508f9d07bfe7f8ac67342505d7619c0
Bikal <bikaladhikari5@gmail.com>
2024-04-17 15:21:15 +1000

    Complete

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from "express";
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Me</h1><p>My name is Bikal</p>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Me</h1><p>Phone: +61123456789</p>");
+});
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`Server running on port at http://localhost:${PORT}.`);
 });
